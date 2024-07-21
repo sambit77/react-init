12d4481f1a02dcbf883fbe0e6ba4a09fbf32535f
Sambit Pradhan <sambitgulu@gmail.com>
2024-07-22 03:36:22 +0530

    basic

---
+++ 01basicapp/src/App.js
@@ -0,0 +1,12 @@
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+   <>
+   <h1>Hellow World React App</h1>
+   <Youtube></Youtube>
+   </>
+  );
+}
+
+export default App;
---
+++ 01basicappvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube"
+function App() {
+
+  return (
+    <>
+    <h1>Hellow World Vite App</h1>
+    <h1>Hellow World Vite App</h1>
+    <Youtube/>
+    </>
+      
+  )
+}
+
+export default App
